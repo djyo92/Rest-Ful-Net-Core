99bdac85bf19663b6825ff1ba2495cac2ed819a2
Dery Douglas Alizares Camacho <dj.yo92@gmail.com>
2020-05-01 16:01:14 -0400

    Filtro JwtBearer

--- MiPrimerWebApiM3/MiPrimerWebApiM3/Startup.cs
+++ MiPrimerWebApiM3/MiPrimerWebApiM3/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,7 @@ namespace MiPrimerWebApiM3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
             services.AddDbContext<AplicationDbContext>(options =>
             options.UseNpgsql(Configuration.GetConnectionString("AutoresDbConection")));
             services.AddControllers().AddNewtonsoftJson(options =>
@@ -50,7 +52,7 @@ namespace MiPrimerWebApiM3
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseAuthentication();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
