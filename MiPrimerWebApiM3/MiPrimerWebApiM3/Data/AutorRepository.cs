c2d74c709274cde18685d525a4d39df3b1abb364
Dery Douglas Alizares Camacho <dj.yo92@gmail.com>
2020-05-03 15:53:56 -0400

    Ejecutar Procedimeintos Almacenados

---
+++ MiPrimerWebApiM3/MiPrimerWebApiM3/Data/AutorRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MiPrimerWebApiM3.DataContext;
+using MiPrimerWebApiM3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimerWebApiM3.Data
+{
+    public class AutorRepository
+    {
+        private readonly ILogger<AutorRepository> logger;
+        private readonly AplicationDbContext context;
+
+        public AutorRepository(ILogger<AutorRepository> logger, AplicationDbContext context)
+        {
+            this.logger = logger;
+            this.context = context;
+        }
+        public async Task<List<AutorDTO>> GetAll()
+        {
+            //throw new Exception("Ocurrio una excepcion");
+            logger.LogInformation("Obteniendo los autores Data Reposit");
+            //context.Database.
+            throw new NotImplementedException();
+        }
+    }
+}
